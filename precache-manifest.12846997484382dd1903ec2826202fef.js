4686b913c2da4018d6a6baee46a04ae05c04525f
anku1612 <sharmaanku908@gmail.com>
2019-07-17 18:14:16 +0530

    Updates

---
+++ precache-manifest.12846997484382dd1903ec2826202fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac1a0057aaaec24d745beaa46ce2091",
+    "url": "/task/index.html"
+  },
+  {
+    "revision": "b5799599e3def473fc33",
+    "url": "/task/static/css/main.a2654c6d.chunk.css"
+  },
+  {
+    "revision": "3865e85bdecabb9d260c",
+    "url": "/task/static/js/2.273c3e5d.chunk.js"
+  },
+  {
+    "revision": "b5799599e3def473fc33",
+    "url": "/task/static/js/main.c7a2d9a0.chunk.js"
+  },
+  {
+    "revision": "f715dc4af397f643c076",
+    "url": "/task/static/js/runtime~main.f79bd0da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/task/static/media/logo.5d5d9eef.svg"
+  }
+]);
